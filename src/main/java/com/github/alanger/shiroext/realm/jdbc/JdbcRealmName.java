be899719701dc70dc0aff3c593d783589d84bff4
a-langer <langeress@gmail.com>
2022-10-18 18:36:44 +0700

    Added findByPassword

--- src/main/java/com/github/alanger/shiroext/realm/jdbc/JdbcRealmName.java
+++ src/main/java/com/github/alanger/shiroext/realm/jdbc/JdbcRealmName.java
@@ -8,14 +8,20 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import org.apache.shiro.authc.AccountException;
 import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.AuthenticationInfo;
 import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authc.SimpleAuthenticationInfo;
+import org.apache.shiro.authc.UnknownAccountException;
 import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.authz.AuthorizationInfo;
 import org.apache.shiro.authz.SimpleAuthorizationInfo;
+import org.apache.shiro.codec.Base64;
+import org.apache.shiro.config.ConfigurationException;
 import org.apache.shiro.realm.jdbc.JdbcRealm;
 import org.apache.shiro.subject.PrincipalCollection;
+import org.apache.shiro.util.ByteSource;
 import org.apache.shiro.util.JdbcUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -44,6 +50,10 @@ public class JdbcRealmName extends JdbcRealm
 
     protected AuthenticationToken getAuthenticationToken(AuthenticationToken token, String nameAttribute)
             throws AuthenticationException {
+
+        if (logger.isTraceEnabled())
+            logger.trace("token: {}, nameAttribute: {}, findByPassword: {}", token, nameAttribute, findByPassword);
+
         UsernamePasswordToken upToken;
         if (token instanceof UsernamePasswordToken) {
             upToken = (UsernamePasswordToken) token;
@@ -61,10 +71,15 @@ public class JdbcRealmName extends JdbcRealm
 
             ps = conn.prepareStatement(principalNameQuery);
             ps.setString(1, username);
+
+            String password = new String((char[]) upToken.getCredentials());
             if (findByPassword)
-                ps.setString(2, upToken.getCredentials().toString());
+                ps.setString(2, password);
 
             rs = ps.executeQuery();
+
+            if (logger.isTraceEnabled())
+                logger.trace("principalNameQuery: {}, {}", principalNameQuery, username);
 
             boolean foundResult = false;
 
@@ -113,6 +128,10 @@ public class JdbcRealmName extends JdbcRealm
             nameAttribute = getPrincipalNameAttribute();
         }
 
+        if (logger.isTraceEnabled())
+            logger.trace("Find principal name - skipIfNullAttribute: {}, nameAttribute: {}", skipIfNullAttribute,
+                    nameAttribute);
+
         if (principalNameQuery != null && !(skipIfNullAttribute && nameAttribute == null)) {
             token = getAuthenticationToken(token, nameAttribute);
         }
@@ -120,7 +139,127 @@ public class JdbcRealmName extends JdbcRealm
         if (!(token instanceof UsernamePasswordToken)) {
             token = new UsernamePasswordToken(token.getPrincipal().toString(), (char[]) token.getCredentials());
         }
-        return super.doGetAuthenticationInfo(token);
+        return findAuthenticationInfo(token);
+    }
+
+    protected AuthenticationInfo findAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
+
+        UsernamePasswordToken upToken = (UsernamePasswordToken) token;
+        String username = upToken.getUsername();
+
+        // Null username is invalid
+        if (username == null) {
+            throw new AccountException("Null usernames are not allowed by this realm.");
+        }
+
+        Connection conn = null;
+        SimpleAuthenticationInfo info = null;
+        try {
+            conn = dataSource.getConnection();
+
+            String srcPassword = new String((char[]) upToken.getCredentials());
+            String password = null;
+            String salt = null;
+            switch (saltStyle) {
+                case NO_SALT:
+                    password = getPasswordForUser(conn, username, srcPassword)[0];
+                    break;
+                case CRYPT:
+                    // TODO: separate password and hash from getPasswordForUser[0]
+                    throw new ConfigurationException("Not implemented yet");
+                // break;
+                case COLUMN:
+                    String[] queryResults = getPasswordForUser(conn, username, srcPassword);
+                    password = queryResults[0];
+                    salt = queryResults[1];
+                    break;
+                case EXTERNAL:
+                    password = getPasswordForUser(conn, username, srcPassword)[0];
+                    salt = getSaltForUser(username);
+            }
+
+            if (password == null) {
+                throw new UnknownAccountException("No account found for user [" + username + "]");
+            }
+
+            info = new SimpleAuthenticationInfo(username, password.toCharArray(), getName());
+
+            if (salt != null) {
+                if (saltStyle == SaltStyle.COLUMN && saltIsBase64Encoded) {
+                    info.setCredentialsSalt(ByteSource.Util.bytes(Base64.decode(salt)));
+                } else {
+                    info.setCredentialsSalt(ByteSource.Util.bytes(salt));
+                }
+            }
+
+        } catch (SQLException e) {
+            final String message = "There was a SQL error while authenticating user [" + username + "]";
+            if (logger.isErrorEnabled()) {
+                logger.error(message, e);
+            }
+
+            // Rethrow any SQL errors as an authentication exception
+            throw new AuthenticationException(message, e);
+        } finally {
+            JdbcUtils.closeConnection(conn);
+        }
+
+        return info;
+    }
+
+    private String[] getPasswordForUser(Connection conn, String username, String password) throws SQLException {
+
+        String[] result;
+        boolean returningSeparatedSalt = false;
+        switch (saltStyle) {
+            case NO_SALT:
+            case CRYPT:
+            case EXTERNAL:
+                result = new String[1];
+                break;
+            default:
+                result = new String[2];
+                returningSeparatedSalt = true;
+        }
+
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        try {
+            if (logger.isTraceEnabled())
+                logger.trace("authenticationQuery: {}, username: {}", authenticationQuery, username);
+
+            ps = conn.prepareStatement(authenticationQuery);
+            ps.setString(1, username);
+            if (findByPassword)
+                ps.setString(2, password);
+
+            // Execute query
+            rs = ps.executeQuery();
+
+            // Loop over results - although we are only expecting one result, since
+            // usernames should be unique
+            boolean foundResult = false;
+            while (rs.next()) {
+
+                // Check to ensure only one row is processed
+                if (foundResult) {
+                    throw new AuthenticationException(
+                            "More than one user row found for user [" + username + "]. Usernames must be unique.");
+                }
+
+                result[0] = rs.getString(1);
+                if (returningSeparatedSalt) {
+                    result[1] = rs.getString(2);
+                }
+
+                foundResult = true;
+            }
+        } finally {
+            JdbcUtils.closeResultSet(rs);
+            JdbcUtils.closeStatement(ps);
+        }
+
+        return result;
     }
 
     @Override
